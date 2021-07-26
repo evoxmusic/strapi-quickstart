274b00f50075cfc3dfdd42c1b6091462ba6acee0
Romaric Philogene <evoxmusic@gmail.com>
2021-07-26 14:25:02 +0200

    Initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8edce63196f04b10664ad23ae2d7288e'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97ecef89-dfce-4ab5-93e5-c100cff90ab5'
+};
